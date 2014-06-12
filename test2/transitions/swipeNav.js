8f63daf0f12382031234f435dc41aa5047e3ed96
MissCatLady <citrus.smooth@gmail.com>
2014-06-12 00:11:36 -0400

    swipe nav

---
+++ test2/transitions/swipeNav.js
@@ -0,0 +1,26 @@
+$(function() {      
+  //Enable swiping...
+  $(".navbar1").swipe( {
+  //Generic swipe handler for all directions
+  swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
+          
+  if (window.location.pathname.indexOf("test4.html")>0) {
+    if (direction=="left") {
+      window.location="../test2.html";
+    } else if (direction=="right"){
+      window.location="#";
+    }
+ 
+  } else if (window.location.pathname.indexOf("games")>0) {
+
+  } else if (window.location.pathname.indexOf("settings")>0)
+
+  } else {
+
+  }
+
+  },
+  //Default is 75px, set to 0 for demo so any distance triggers swipe
+  threshold:5
+  });
+});
