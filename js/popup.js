2ef5a134611ed94e26fb09b0b7e788dfcc1a6a76
MissCatLady <citrus.smooth@gmail.com>
2014-06-14 12:21:44 -0400

    added meeting place

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,11 @@
+
+/*
+
+Attribution: Joel Peterson
+Status: Minor Name Edits
+
+*/
+
 function loadPopup(){
 	//loads popup only if it is disabled
 	if($("#bgPopup").data("state")==0){
@@ -5,7 +13,7 @@ function loadPopup(){
 			"opacity": "0.7"
 		});
 		$("#bgPopup").fadeIn("medium");
-		$("#Popup").fadeIn("medium");
+		$("#places-popup").fadeIn("medium");
 		$("#bgPopup").data("state",1);
 	}
 }
@@ -13,7 +21,7 @@ function loadPopup(){
 function disablePopup(){
 	if ($("#bgPopup").data("state")==1){
 		$("#bgPopup").fadeOut("medium");
-		$("#Popup").fadeOut("medium");
+		$("#places-popup").fadeOut("medium");
 		$("#bgPopup").data("state",0);
 	}
 }
@@ -21,9 +29,9 @@ function disablePopup(){
 function centerPopup(){
 	var winw = $(window).width();
 	var winh = $(window).height();
-	var popw = $('#Popup').width();
-	var poph = $('#Popup').height();
-	$("#Popup").css({
+	var popw = $('#places-popup').width();
+	var poph = $('#places-popup').height();
+	$("#places-popup").css({
         "position" : "absolute",
         "top" : winh/2-poph/2,
         "left" : winw/2-popw/2
@@ -36,13 +44,20 @@ function centerPopup(){
 
 $(document).ready(function() {
    $("#bgPopup").data("state",0);
-   $("#myButton").click(function(){
+   $("#places").click(function(){
 		centerPopup();
 		loadPopup();   
    });
    $("#popupClose").click(function(){
 	   	disablePopup();
+	
    });
+
+   $("#bgPopup").click(function(){
+   		disablePopup();
+   });
+
+	
    $(document).keypress(function(e){
 		if(e.keyCode==27) {
 			disablePopup();	
@@ -50,8 +65,10 @@ $(document).ready(function() {
 	});
 });
 
+//Recenter the popup on resize - Thanks @Dan Harvey [http://www.danharvey.com.au/]
 $(window).resize(function() {
 centerPopup();
 });
+
 
 
