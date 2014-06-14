cb30b1702d1d3e125f1c838e66de3eb4f546a795
IlyaSam <ilya.samsonov90@gmail.com>
2014-06-13 21:58:05 -0400

    added time

--- js/chat.js
+++ js/chat.js
@@ -4,10 +4,11 @@ var vis
 $(document).ready(function(){
   $('input[type="submit"]').click(function(){
     if (document.getElementById("message").value != "")
+    var d = new Date()
     $("#messageList").append('<li id="'+ id + 
-			      '"><div><img src="imgs/mashiyat1.jpg"></img></div>:<p>' + 
-			      document.getElementById("message").value +
-			      '</p></li>');
+			      '"><div><img src="imgs/mashiyat1.jpg"></img></div> <span id ="time">'+
+			      d.getHours() + ':' + d.getMinutes()  +'</span><span id ="text">' + 
+			      document.getElementById("message").value + '</span></li>');
     document.getElementById("message").value = "";
     id = id + 1;
   });
