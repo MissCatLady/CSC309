a8c60a4732b07d6f81f83481b13d67225f0fb762
IlyaSam <ilya.samsonov90@gmail.com>
2014-06-15 20:02:39 -0400

    fix copy chat in games

--- js/hidepage.js
+++ js/hidepage.js
@@ -1,14 +1,9 @@
 
 $(document).ready(function() {
-	$('#content-2').hide();
+	$('.game').hide();
 	$('#button-submit').click(function(e) {
 	e.preventDefault();
-	$('#content-1').hide();
-	$('#content-2').show();
-	});
-
-	function show_page2(){	
-	$('#content-1').hide();
-	$('#content-2').show();
-	}	
+	$('.col-l form').hide();
+	$('.game').show();
+	});	
 });
