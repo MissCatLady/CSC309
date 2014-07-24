cf1f2a9a22256896cc241a4d1a70c223a738cc2c
MissCatLady <citrus.smooth@gmail.com>
2014-07-24 00:05:07 -0400

    more stuff

---
+++ public/js/meet.js
@@ -0,0 +1,156 @@
+/*<!--Map change javascript-->*/
+if(typeof(Storage) !== "undefined") {
+} else {
+}
+
+var xmlhttp;
+function loadXMLDoc(url,cfunc)
+{
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=cfunc;
+xmlhttp.open("GET",url,true);
+xmlhttp.send();
+}
+
+$(document).ready(function(){
+  $(".dotstyle ul li a").click(function(){
+    if ($(this).parent().attr("class")!="current") {
+      $("#location div h4").text($(this).text());
+      if($(this).attr("id")=="addfriend"){
+	$("#map-canvas").hide();
+	$("#friendAdd").show();
+      }else{
+	$("#map-canvas").show();
+	$("#friendAdd").hide();
+	if($(this).attr("id")=="mylocation") {
+	  getLocation();
+	} else {
+	  attendeeLocation($(this).attr("id"));
+	}
+      }
+      $(".current").removeClass("current");
+      $(this).parent().addClass("current");
+    }
+  });
+});
+
+$(document).ready(function(){
+   $("#search_button").click(function () {
+      $(".current").removeClass("current");
+		$.get("/center", function(data) {
+			var pos = new google.maps.LatLng(data[0], data[1])
+		   map = new google.maps.Map(document.getElementById("map-canvas"));
+		   map.setZoom(16);
+		   map.setCenter(pos);
+			var request = {
+		 		location: pos,
+		 		radius: '50',
+		 		query: [$("#search_text").val()]
+		 	};
+			service = new google.maps.places.PlacesService(map);
+	  		service.textSearch(request, function (results, status) {
+	  			if (status == google.maps.places.PlacesServiceStatus.OK) {
+		 			for (var i = 0; i < results.length; i++) {
+		   			var place = results[i];
+		   			var marker = new google.maps.Marker({
+		   				map: map,
+		   				position: place.geometry.location,
+							title: place.name
+		 				});
+						var content = "<div><form method='POST' action='/suggest'><input type='submit' value='suggest'>" +
+											"<input type='hidden' value=" + place.name + " name = 'location_name'>" +
+											"<input type='hidden' value=" + place.place_id + " name = 'place_id'></form></div>"
+    					attachEvent(marker, content);
+		 			}
+	  			}
+			});
+		});
+	});
+});
+
+function attachEvent(marker, message) {
+	var infowindow = new google.maps.InfoWindow({
+		content: message
+	});
+
+	google.maps.event.addListener(marker, 'click', function() {
+		infowindow.open(marker.get('map'), marker);
+	});
+}
+$(document).ready(function(){
+   $("#friendAdd").hide();
+   getLocation();
+});
+function getLocation(){
+  // Try HTML5 geolocation
+	if(navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			sendLocation(position.coords.latitude, position.coords.longitude);
+			var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+			map = new google.maps.Map(document.getElementById("map-canvas"));
+			map.setZoom(16);
+			map.setCenter(pos);
+			$.get("/center", function(data) {
+				if (data[2] == "undefined") {
+					var marker = new google.maps.Marker({
+						map: map,
+						position: pos,
+						title:"Your'e here"
+				 	});
+				} else {
+					var request = {
+					 	placeId: data[2]
+					};
+					service = new google.maps.places.PlacesService(map);
+					service.getDetails(request, function(place, status) {
+						if (status == google.maps.places.PlacesServiceStatus.OK) {
+							var directionsService = new google.maps.DirectionsService();
+							var directionsDisplay = new google.maps.DirectionsRenderer();
+  							directionsDisplay.setMap(map);
+							var start = pos;
+							var end = place.geometry.location;
+							var request = {
+								origin:start,
+								destination:end,
+								travelMode: google.maps.TravelMode.DRIVING
+							};
+							directionsService.route(request, function(response, status) {
+								if (status == google.maps.DirectionsStatus.OK) {
+									directionsDisplay.setDirections(response);
+								}
+							});
+					  	}
+					});
+				}
+			});
+		});
+	}
+}
+
+function sendLocation(latitude,longitude) {
+  $.post("/location", {latitude:latitude, longitude:longitude}, function(data) {
+    if (data) {;
+    }
+  });
+}
+ 
+function attendeeLocation(name) {
+  $.get("/location", {name:name}, function(data) {
+		var pos = new google.maps.LatLng(data[0], data[1]);
+		map = new google.maps.Map(document.getElementById("map-canvas")); 
+		map.setZoom(16);      // This will trigger a zoom_changed on the map
+		map.setCenter(pos);
+		var marker = new google.maps.Marker({
+      	map: map,
+      	position: pos,
+   		title: name + " is here"
+    	});
+  });
+}
---
+++ public/js/settings.js
@@ -0,0 +1,234 @@
+$(document).ready(function(){
+	
+	//create drop down for months
+	var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September" ,"October", "November", "December"];
+	for(i = 0; i < months.length; i++) {
+		var month = months[i];
+		var elem = document.createElement("OPTION");
+		elem.text = month;
+		elem.value = month;
+		document.getElementById("myMonth").appendChild(elem);
+	};
+	// create drop down for days
+	var days = range(1,32);
+	for(i = 0; i < days.length; i++) {
+		var day = days[i];
+		var elem = document.createElement("OPTION");
+		elem.text = day;
+		elem.value = day;
+		document.getElementById("myDays").appendChild(elem);
+	};
+	
+	//create drop down for years
+	var years = range(2014,1905, -1);
+	for(i = 0; i < years.length; i++) {
+		var year = years[i];
+		var elem = document.createElement("OPTION");
+		elem.text = year;
+		elem.value = year;
+		document.getElementById("myYears").appendChild(elem);
+	};
+	window.localStorage.setItem('themeDisplay', "noTheme");
+	document.getElementById("myMonth").value = document.getElementById("mValue").value;
+	document.getElementById("myDays").value = document.getElementById("dValue").value;
+	document.getElementById("myYears").value = document.getElementById("yValue").value;
+	// loaded profile settings from DB
+	// loaded privacy settings from DB
+	// loaded theme settings from DB
+});
+
+$(document).ready(function(){
+	$("#female").click(function(){
+		console.log("female");
+		var gen = document.getElementById("hiddenGender");
+		gen.value = "female";
+		
+	});
+});
+$(document).ready(function(){
+	$("#male").click(function(){
+		console.log("male");
+		var gen = document.getElementById("hiddenGender");
+		gen.value = "male";
+	});
+});
+// question 1
+$(document).ready(function(){
+	$("#radio1").click(function(){
+		console.log("q1everyone");
+		var q1 = document.getElementById("question1");
+		q1.value = "E";
+	});
+});
+
+$(document).ready(function(){
+	$("#radio2").click(function(){
+		console.log("q1filter");
+		console.log(document.getElementById("question1").value);
+		var q1 = document.getElementById("question1");
+		q1.value = "F";
+		console.log(document.getElementById("question1").value);
+	});
+});
+
+// question 2
+$(document).ready(function(){
+	$("#radio3").click(function(){
+		console.log("q2everyone");
+		var q2 = document.getElementById("question2");
+		q2.value = "E";
+	});
+});
+
+$(document).ready(function(){
+	$("#radio4").click(function(){
+		console.log("q2filter");
+		console.log(document.getElementById("question2").value);
+		var q2 = document.getElementById("question2");
+		q2.value = "F";
+		console.log(document.getElementById("question2").value);
+	});
+});
+
+// question 3
+$(document).ready(function(){
+	$("#radio5").click(function(){
+		console.log("q3everyone");
+		var q3 = document.getElementById("question3");
+		q3.value = "E";
+	});
+});
+
+$(document).ready(function(){
+	$("#radio6").click(function(){
+		console.log("q3filter");
+		var q1 = document.getElementById("question3");
+		q1.value = "F";
+	});
+});
+
+// question 4
+$(document).ready(function(){
+	$("#radio7").click(function(){
+		console.log("q4everyone");
+		var q4 = document.getElementById("question4");
+		q4.value = "E";
+	});
+});
+
+$(document).ready(function(){
+	$("#radio8").click(function(){
+		console.log("q4filter");
+		var q4 = document.getElementById("question4");
+		q4.value = "F";
+	});
+});
+// themes
+$(document).ready(function(){
+	$("#radio9").click(function(){
+		console.log("theme1");
+		var t = document.getElementById("selectTheme");
+		t.value = "theme1";
+		var theme = document.getElementById("tempThemeDisplay");
+		theme.href = "../css/theme1.css";
+		window.localStorage.themeDisplay = "../css/theme1.css";
+	});
+});
+
+$(document).ready(function(){
+	$("#radio10").click(function(){
+		console.log("theme2");
+		var t = document.getElementById("selectTheme");
+		t.value = "theme2";
+		var theme = document.getElementById("tempThemeDisplay");
+		theme.href = "../css/theme2.css";
+		window.localStorage.themeDisplay = "../css/theme2.css";
+	});
+});
+
+$(document).ready(function(){
+	$("#radio11").click(function(){
+		console.log("theme3");
+		var t = document.getElementById("selectTheme");
+		t.value = "theme3";
+		var theme = document.getElementById("tempThemeDisplay");
+		theme.href = "../css/theme3.css";
+		window.localStorage.themeDisplay = "../css/theme3.css";
+	});
+});
+
+$(document).ready(function(){
+	$("#radio12").click(function(){
+		console.log("theme4");
+		var t = document.getElementById("selectTheme");
+		t.value = "theme4";
+		var theme = document.getElementById("tempThemeDisplay");
+		theme.href = "../css/theme4.css";
+		window.localStorage.themeDisplay = "../css/theme4.css";
+	});
+});
+$(document).ready(function(){
+	$("#radio13").click(function(){
+		console.log("theme5");
+		var t = document.getElementById("selectTheme");
+		t.value = "theme5";
+		
+		var theme = document.getElementById("tempThemeDisplay");
+		theme.href = "../css/theme5.css";
+		window.localStorage.themeDisplay = "../css/theme5.css";
+		
+	});
+});
+
+$(document).ready(function(){
+	$("#radio14").click(function(){
+		console.log("theme6");
+		var t = document.getElementById("selectTheme");
+		t.value = "theme6";
+		
+		var theme = document.getElementById("tempThemeDisplay");
+		theme.href = "../css/theme6.css";
+		window.localStorage.themeDisplay = "../css/theme6.css";
+	});
+});
+
+$(document).ready(function(){
+	$("privacySave").click(function(){
+		console.log("privacy save clicked");
+	});
+});
+/**
+* Underscore is an open-source component of DocumentCloud. 
+* Underscore.js 1.6.0
+* http://underscorejs.org
+* (c) 2009-2014 Jeremy Ashkenas, DocumentCloud and Investigative Reporters & Editors
+* Underscore may be freely distributed under the MIT license.
+*/
+range=function(n,t,r){
+
+arguments.length<=1&&(t=n||0,n=0),r=arguments[2]||1;
+for(var e=Math.max(Math.ceil((t-n)/r),0),u=0,i=new Array(e);e>u;)i[u++]=n,n+=r;return i}
+
+
+
+/*$(document).ready(function(){
+	$("#profileSave").click(profileSave);
+	$("#privacySave").click(privacySave);
+	document.getElementById("radio9").addEventListener('click',function() {
+	console.log("fdf");
+
+	
+	});
+});*/
+profileSave=function(){
+var fn = document.getElementById("FN");
+console.log(fn.value);
+	window.localStorage.setItem('Fname', fn.value);}
+/*
+	var month = document.getElementById("myMonth")
+	month.value = "May";
+	var day = document.getElementById("myDays")
+	day.value = "30";
+	var year = document.getElementById("myYears")
+	year.value = "1989";*/
+
